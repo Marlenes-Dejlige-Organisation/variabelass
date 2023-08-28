3bc507b2233fa6329925a7e982aaff5e9b0c5528
MarlaRey <hejmarlenehej@gmail.com>
2023-08-28 12:33:51 +0200

    jeg har lige tilføjet hvad opgaverne går ud på

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,31 @@
-//1
+// 3: Opret en variabel og tildel den en værdi:
+// Opret en variabel kaldet navn og tildel den dit eget navn.
+// Derefter skal du bruge console.log til at udskrive værdien af variablen navn til konsollen.
+
 let navn = "Marlene";
 console.log(navn);
 
-//2
+// 2: Beregning med variabler:
+// Opret to variabler, tal1 og tal2, og tildel dem to forskellige numeriske værdier.
+// Lav derefter en variabel kaldet sum og tildel den summen af tal1 og tal2. Brug console.log til at udskrive værdien af sum til konsollen.
+
 let tal1 = 10;
 let tal2 = 5;
 let sum = tal1 + tal2;
 
 console.log(sum);
 
-//3
+// 3: Ændre værdien af en variabel:
+// Opret en variabel kaldet alder og tildel den din alder.
+// Derefter skal du ændre værdien af alder til at være 5 år ældre og udskrive den nye værdi til konsollen.
 let alder = 40; 
 alder = alder + 5;
 
 console.log(alder);
 
-//4
+//4:
 let myNumber1 = 1;
 
-//5
 // Opret en variabel kaldet myNumber med værdien 1
 let myNumber = 1;
 
