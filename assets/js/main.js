f275b041e2edd75a61c11c77bf3dee97c39530e2
MarlaRey <hejmarlenehej@gmail.com>
2023-08-28 12:25:28 +0200

    jeg har lavet 2. del

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+//1
+let navn = "Marlene";
+console.log(navn);
+
+//2
+let tal1 = 10;
+let tal2 = 5;
+let sum = tal1 + tal2;
+
+console.log(sum);
+
+//3
+let alder = 40; 
+alder = alder + 5;
+
+console.log(alder);
+
+//4
+let myNumber = 1;
+
+//5
+// Opret en variabel kaldet myNumber med værdien 1
+let myNumber = 1;
+
+// Increment myNumber med 1
+myNumber++;
+
+// Udskriv den nye værdi til konsollen
+console.log("Efter increment:", myNumber);
+
+// Decrement myNumber med 1
+myNumber--;
+
+// Udskriv den nye værdi til konsollen
+console.log("Efter decrement:", myNumber);
